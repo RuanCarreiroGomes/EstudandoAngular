0c38eacca0cf73d7d12b1cf50abd70a504c6a900
Ruan Victor <62728646+RuanCarreiroGomes@users.noreply.github.com>
2023-03-06 10:26:19 -0300

    Trabalhando com rotas usando a bibliteca Angular Router.

---
+++ roteamento/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
+
+const routes: Routes = [
+  {path: "primeira-pagina", component: PrimeiraPaginaComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
