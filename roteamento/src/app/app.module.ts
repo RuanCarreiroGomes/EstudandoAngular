e565b9b94643c7cb21888a9805acce8082c15062
Ruan Victor <62728646+RuanCarreiroGomes@users.noreply.github.com>
2023-03-09 14:15:16 -0300

    Trabalhando com parâmetros de rotas.

--- roteamento/src/app/app.module.ts
+++ roteamento/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
 import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
 import { PaginaNotFoundComponent } from './pagina-not-found/pagina-not-found.component';
+import { PaginaComParametrosComponent } from './pagina-com-parametros/pagina-com-parametros.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PrimeiraPaginaComponent,
     SegundaPaginaComponent,
-    PaginaNotFoundComponent
+    PaginaNotFoundComponent,
+    PaginaComParametrosComponent
   ],
   imports: [
     BrowserModule,
