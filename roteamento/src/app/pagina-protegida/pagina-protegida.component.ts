8fbb9f9f733bd2fed45b938ddaf5643d8cfa02c7
Ruan Victor <62728646+RuanCarreiroGomes@users.noreply.github.com>
2023-03-12 17:30:18 -0300

    Implementando segurança em páginas com o Auth Guard e CanActivate.

---
+++ roteamento/src/app/pagina-protegida/pagina-protegida.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-pagina-protegida',
+  templateUrl: './pagina-protegida.component.html',
+  styleUrls: ['./pagina-protegida.component.css']
+})
+export class PaginaProtegidaComponent {
+  constructor(
+    private router: Router,
+    private auth: AuthService
+  ){}
+  
+  logout(){
+    this.auth.logout();
+    this.router.navigate([""]);
+  }
+}
